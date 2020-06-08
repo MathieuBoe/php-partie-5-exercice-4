f392b2edc951b1725a856398809efe2b89bac972
MathieuBoe <mathieuboe60@gmail.com>
2020-06-08 11:18:11 +0200

    Exercice complet

---
+++ Exercice4.php
@@ -0,0 +1,10 @@
+<?php
+$number1 = 12;
+$number2 = 9;
+$title = 'Exercice4';
+
+include 'header.php';
+include '../Exercice1/Exercice1.php';
+$months[9] = 'Août';
+echo $months[9];
+include 'footer.php';?>
